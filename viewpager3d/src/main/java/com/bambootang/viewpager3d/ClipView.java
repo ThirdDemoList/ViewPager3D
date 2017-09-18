28ad9029282a20a9259ed4b91dd920f2c556bc14
唐帅 <tangshuai@talkweb.com.cn>
2017-09-19 01:53:53 +0800

    修改library名称

--- viewpager3d/src/main/java/com/bambootang/viewpager3d/ClipView.java
+++ viewpager3d/src/main/java/com/bambootang/viewpager3d/ClipView.java
@@ -5,8 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
@@ -23,15 +21,14 @@ public class ClipView extends LinearLayout implements Clipable {
         super(context);
     }
 
-    public ClipView(Context context, @Nullable AttributeSet attrs) {
+    public ClipView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public ClipView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public ClipView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public ClipView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
